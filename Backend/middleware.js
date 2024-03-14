ae4f22dbaf75026d5779528a53842b124c616e37
@AamirK23 <aamirk3644@gmail.com>
2024-03-14 15:11:55 +0530

    Added Authentication middleware

---
+++ Backend/middleware.js
@@ -0,0 +1,19 @@
+const { JWT_SECRET } = require('./config');
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) =>{
+    const authHeaders = req.headers.authorization;
+
+    if(!authHeaders || !authHeaders.startsWith('Bearer ')){
+        return res.status(403).json();
+    }
+
+    const token = authHeaders.split(' ')[1];
+    try{
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch(err) {
+        return res.status(403).json({});
+    }
+}
